9be9bc9c4aa6abd48b5145d53dc102f42621810c
MCCUZCANO <i201821727@cibertec.edu.pe>
2021-09-30 11:05:28 -0500

    Arreglos

--- src/app/components/add-proveedor/add-proveedor.component.ts
+++ src/app/components/add-proveedor/add-proveedor.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { Proveedor } from 'src/app/models/proveedor.model';
+import { Ubigeo } from 'src/app/models/ubigeo.model';
+import { ProveedorService } from 'src/app/services/proveedor.service';
+import { UbigeoService } from 'src/app/services/ubigeo.service';
 
 @Component({
   selector: 'app-add-proveedor',
   templateUrl: './add-proveedor.component.html',
-  styleUrls: ['./add-proveedor.component.css']
+  styleUrls: ['./add-proveedor.component.css'],
 })
 export class AddProveedorComponent implements OnInit {
+  departamentos: string[] = [];
+  provincias: string[] = [];
+  distritos: Ubigeo[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  proveedor: Proveedor = {
+    ubigeo: {
+      idUbigeo: -1,
+      departamento: '-1',
+      provincia: '-1',
+      distrito: '',
+    },
+  };
+  constructor(
+    private ubigeoService: UbigeoService,
+    private proveedorService: ProveedorService
+  ) {
+    this.ubigeoService
+      .listarDepartamento()
+      .subscribe((departamentos) => (this.departamentos = departamentos));
   }
 
+  registroProveedor() {
+    console.log(this.proveedor);
+    this.proveedorService.registroProveedor(this.proveedor).subscribe(
+      (response) => {
+        console.log(response.mensaje);
+        alert(response.mensaje);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  listaProvincia() {
+    this.ubigeoService
+      .listaProvincias(this.proveedor.ubigeo?.departamento)
+      .subscribe((provincias) => (this.provincias = provincias));
+  }
+
+  listaDistrito() {
+    this.ubigeoService
+      .listaDistritos(
+        this.proveedor.ubigeo?.departamento,
+        this.proveedor.ubigeo?.provincia
+      )
+      .subscribe((distritos) => (this.distritos = distritos));
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Proveedor } from '../models/proveedor.model';
 
+const baseUrl = 'http://localhost:8090/rest/proveedor';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProveedorService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  registroProveedor(data: Proveedor): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
 }
